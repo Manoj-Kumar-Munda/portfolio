411c54a2dbe2af200468521b1a4e5bd9064f6215
Manoj Kumar Munda <manojkrmunda828@gmail.com>
2025-10-17 23:52:12 +0530

    feat: added custom glass styles

--- app/page.tsx
+++ app/page.tsx
@@ -4,15 +4,24 @@ import Blogs from "./blogs/_components/blogs";
 import Heading from "@/components/heading";
 import Subheading from "@/components/subheading";
 import { projectList } from "@/constants";
+import { ReactNode } from "react";
+
+export const GlassEffect = ({ children }: { children: ReactNode }) => {
+  return (
+    <span className="relative inline-block rounded-md [background-image:linear-gradient(to_bottom,var(--color-gray-200),transparent_8px,var(--color-gray-200)_26px)] px-2 py-0.5 text-neutral-500">
+      {children}
+    </span>
+  );
+};
 
 export default function Home() {
   return (
     <Container className="min-h-screen p-4 md:py-10">
       <Heading>Manoj Kr. Munda</Heading>
       <Subheading>
-        I am a software engineer with a passion for building web applications
-        and exploring new technologies. I'm currently working as a frontend
-        engineer at Google.
+        I am a <GlassEffect>software engineer</GlassEffect> with a passion for
+        building web applications and exploring new technologies. Have 1+ years
+        of experience of working at a startup as a frontend engineer.
       </Subheading>
       <div className="py-8">
         <Projects projects={projectList.slice(0, 3)} />
