82a05913e830995f3e64df3b8bc29d1d0c32de84
Manoj Kumar Munda <manojkrmunda828@gmail.com>
2025-10-18 11:49:53 +0530

    refactor: layout changes done

--- app/projects/layout.tsx
+++ app/projects/layout.tsx
@@ -2,7 +2,7 @@ import Container from "@/components/container";
 import { ReactNode } from "react";
 
 const ProjectLayout = ({ children }: { children: ReactNode }) => {
-  return <Container className="min-h-svh p-6 md:py-10">{children}</Container>;
+  return <Container className="min-h-svh md:py-10">{children}</Container>;
 };
 
 export default ProjectLayout;
--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,10 +1,20 @@
 import Heading from "@/components/heading";
 import Projects from "@/components/projects";
+import Separator from "@/components/separator";
+import Subheading from "@/components/subheading";
 const ProjectsPage = () => {
   return (
     <>
       <Heading>Projects</Heading>
+      <Subheading>
+        I'm a passionate software engineer dedicated to crafting elegant
+        solutions for complex problems. With expertise in full-stack
+        development, I enjoy building user-centric applications that make a
+        difference.
+      </Subheading>
+      <Separator />
       <Projects />
+      <Separator />
     </>
   );
 };
