8d30cddd82c001bb94ae0287c92f90912ba69e29
Manoj Kumar Munda <manojkrmunda828@gmail.com>
2025-10-19 20:52:07 +0530

    chore: confiigure svgr

--- next.config.ts
+++ next.config.ts
@@ -4,19 +4,49 @@ import withMDX from "@next/mdx";
 //next config setup for next-mdx-remote
 
 const nextConfig: NextConfig = {
+  turbopack: {
+    rules: {
+      "*.svg": {
+        loaders: [{ loader: "@svgr/webpack", options: { icon: true } }],
+        as: "*.js",
+      },
+    },
+  },
   pageExtensions: ["js", "jsx", "mdx", "ts", "tsx"],
   transpilePackages: ["next-mdx-remote"],
   images: {
     remotePatterns: [
       {
         protocol: "https",
-        hostname: "placehold.co",
-      },
-      {
-        protocol: "https",
         hostname: "cdn-images-1.medium.com",
       },
     ],
+  },
+  webpack(config) {
+    const fileLoaderRule = config.module.rules.find((rule: any) =>
+      rule.test?.test?.(".svg"),
+    );
+
+    config.module.rules.push(
+      // Reapply file loader for ?url usage (e.g., import logoUrl from './logo.svg?url')
+      {
+        ...fileLoaderRule,
+        test: /\.svg$/i,
+        resourceQuery: /url/, // *.svg?url
+      },
+      // Default: treat .svg as React components
+      {
+        test: /\.svg$/i,
+        issuer: fileLoaderRule.issuer,
+        resourceQuery: { not: [...fileLoaderRule.resourceQuery.not, /url/] },
+        use: ["@svgr/webpack"],
+      },
+    );
+
+    // Exclude .svg from the original file loader
+    fileLoaderRule.exclude = /\.svg$/i;
+
+    return config;
   },
 };
 
